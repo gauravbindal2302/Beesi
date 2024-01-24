a95000ac561d5129abeab7e0afe131a6718c24f5
Gaurav Bindal <gb2302bindal@gmail.com>
2024-01-24 16:42:11 +0530

    Push files to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const steps = [
+  "Set your goal",
+  "Create or Join Group",
+  "Pay amount on time",
+  "Earn Rewards",
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app-header">
+        <h1>How It Works</h1>
+      </div>
+      <div className="app-content">
+        {steps.map((step, index) => (
+          <div className="step-container" key={index}>
+            <div className="ping"></div>
+            <div className="steps">
+              <img src={`${index + 1}.png`} alt="" />
+              <p>Step {index + 1}:</p>
+              <p>{step}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
